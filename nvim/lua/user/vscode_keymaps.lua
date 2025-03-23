9ce5da53b9914882cf8d07902a576b6c3b4c2a12
kovacsgellert <kovacsgellertpal@gmail.com>
2025-03-23 21:02:20 +0200

    Add neovim, update some others.

---
+++ nvim/lua/user/vscode_keymaps.lua
@@ -0,0 +1,38 @@
+local keymap = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
+-- remap leader key
+keymap("n", "<Space>", "", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- yank to system clipboard
+keymap({"n", "v"}, "<leader>y", '"+y', opts)
+
+-- paste from system clipboard
+keymap({"n", "v"}, "<leader>p", '"+p', opts)
+
+-- better indent handling
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- move text up and down
+keymap("v", "J", ":m .+1<CR>==", opts)
+keymap("v", "K", ":m .-2<CR>==", opts)
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+
+-- paste preserves primal yanked piece
+keymap("v", "p", '"_dP', opts)
+
+-- removes highlighting after escaping vim search
+keymap("n", "<Esc>", "<Esc>:noh<CR>", opts)
+
+-- general keymaps
+keymap({"n", "v"}, "<leader>t", "<cmd>lua require('vscode').action('workbench.action.terminal.toggleTerminal')<CR>")
+keymap({"n", "v"}, "<leader>b", "<cmd>lua require('vscode').action('editor.debug.action.toggleBreakpoint')<CR>")
+keymap({"n", "v"}, "<leader>fd", "<cmd>lua require('vscode').action('editor.action.formatDocument')<CR>")
+keymap({"n", "v"}, "gd", "<cmd>lua require('vscode').action('editor.action.revealDefinition')<CR>")
+keymap({"n", "v"}, "gi", "<cmd>lua require('vscode').action('editor.action.goToImplementation')<CR>")
+keymap({"n", "v"}, "gr", "<cmd>lua require('vscode').action('editor.action.referenceSearch.trigger')<CR>")
+keymap({"n", "v"}, "<leader>ai", "<cmd>lua require('vscode').action('workbench.panel.chat')<CR>")
