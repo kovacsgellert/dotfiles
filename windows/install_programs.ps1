6717d2bfa81bcf4e00f3c851055a2f2179218194
kovacsgellert <kovacsgellertpal@gmail.com>
2025-08-13 14:37:49 +0300

    Update list of programs to install on Windows setup.

--- windows/install_programs.ps1
+++ windows/install_programs.ps1
@@ -5,7 +5,6 @@ winget install --id bambulab.BambuStudio -e --accept-package-agreements --accept
 winget install --id Notion.Notion -e --accept-package-agreements --accept-source-agreements
 winget install --id WinSCP.WinSCP -e --accept-package-agreements --accept-source-agreements
 winget install --id 7zip.7zip -e --accept-package-agreements --accept-source-agreements
-winget install --id curl.curl -e --accept-package-agreements --accept-source-agreements
 winget install --id Microsoft.PowerToys -e --accept-package-agreements --accept-source-agreements
 winget install --id jtroo.kanata_gui -e --accept-package-agreements --accept-source-agreements
 winget install --id Microsoft.PowerShell -e --accept-package-agreements --accept-source-agreements
@@ -26,7 +25,15 @@ winget install --id Canonical.Ubuntu.2204 -e --accept-package-agreements --accep
 winget install --id Microsoft.DotNet.SDK.9 -e --accept-package-agreements --accept-source-agreements
 winget install --id Starship.Starship -e --accept-package-agreements --accept-source-agreements
 winget install --id Neovim.Neovim -e --accept-package-agreements --accept-source-agreements
-winget install --id Microsoft.VisualStudio.2022.Community -e --accept-package-agreements --accept-source-agreements --override "--add Microsoft.VisualStudio.Workload.NetWeb --add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.Azure"
 winget install --id Bruno.Bruno -e --accept-package-agreements --accept-source-agreements
+winget install --id Microsoft.VisualStudioCode --override "/verysilent /suppressmsgboxes /mergetasks='!runcode,addcontextmenufiles,addcontextmenufolders,associatewithfiles,addtopath'"
+#winget install --id Microsoft.VisualStudio.2022.Community -e --accept-package-agreements --accept-source-agreements --override "--add Microsoft.VisualStudio.Workload.NetWeb --add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.Azure"
 
-#winget install Microsoft.VisualStudioCode --override "/verysilent /suppressmsgboxes /mergetasks='!runcode,addcontextmenufiles,addcontextmenufolders,associatewithfiles,addtopath'"
+winget install --id curl.curl -e --accept-package-agreements --accept-source-agreements
+winget install --id junegunn.fzf
+winget install --id JesseDuffield.lazygit -e
+winget install --id BurntSushi.ripgrep.MSVC
+winget install --id sharkdp.fd
+winget install --id ajeetdsouza.zoxide
+
+
